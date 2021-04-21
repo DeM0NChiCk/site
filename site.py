d8640ac8145395d33ee3a4a906f27d887936a7c4
DeM0NChiCk <82106466+DeM0NChiCk@users.noreply.github.com>
2021-04-21 19:43:53 +0300

    Add files via upload

--- site.py
+++ site.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template, url_for, request, redirect
 import os
 
-app = Flask(__name__)
-app.add_url_rule('/', 'index', index)
-app.run
+def katalog():
+    return render_template('katalog.html')
+
+def str2():
+    return render_template('str2.html')
+
+folder = os.getcwd()
+
+app=Flask(__name__, template_folder=folder, static_folder=folder)
+
+app.add_url_rule('/', 'katalog', katalog) # methods = ["post", 'get'])
+
+#app.add_url_rule('/', 'str2', str2) # methods = ["post", 'get'])
+
+
+
+app.run()
